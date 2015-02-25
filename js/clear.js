5a30fc706a77bf2fcea57ef6bc7d7ee482353e55
Cody Squadroni <codysquadroni@weber.edu>
2015-02-25 10:09:49 -0700

    created a simple drawing app using canvas

---
+++ js/clear.js
@@ -0,0 +1,3 @@
+document.getElementById('clear').addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}, false);
---
+++ js/radius.js
@@ -0,0 +1,32 @@
+var setRadius = function(newRadius) {
+    if(newRadius < minRad) {
+        newRadius = minRad;
+    }
+    else if (newRadius > maxRad){
+        newRadius = maxRad;
+    }
+    radius = newRadius;
+    context.lineWidth = radius*2;
+    radSpan.innerHTML = radius;
+};
+
+var minRad = 0.5,
+    maxRad = 100,
+    defaultRad = 20,
+    interval = 5,
+    radSpan = document.getElementById('radVal'),
+    decRad = document.getElementById('decRad'),
+    incRad = document.getElementById('incRad');
+
+decRad.addEventListener('click', function(){
+    setRadius(radius-interval);
+});
+
+incRad.addEventListener('click', function(){
+    setRadius(radius+interval);
+});
+
+setRadius(defaultRad);
+
+
+
